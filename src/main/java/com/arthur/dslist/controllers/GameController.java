97dc0fcfaf0ede768517425f25ad7f38bb59ab73
Arthur Lage <arthurlage2006@gmail.com>
2023-05-14 22:46:43 -0300

    get game by id

--- src/main/java/com/arthur/dslist/controllers/GameController.java
+++ src/main/java/com/arthur/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.arthur.dslist.dto.GameDTO;
 import com.arthur.dslist.dto.GameMinDTO;
 import com.arthur.dslist.services.GameService;
 
@@ -23,5 +25,10 @@ public class GameController {
   @GetMapping
   public List<GameMinDTO> findAll() {
     return this.gameService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public GameDTO findById(@PathVariable Long id) {
+    return this.gameService.findById(id);
   }
 }
--- src/main/java/com/arthur/dslist/services/GameService.java
+++ src/main/java/com/arthur/dslist/services/GameService.java
@@ -3,7 +3,9 @@ package com.arthur.dslist.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.arthur.dslist.dto.GameDTO;
 import com.arthur.dslist.dto.GameMinDTO;
 import com.arthur.dslist.repositories.GameRepository;
 
@@ -15,7 +17,14 @@ public class GameService {
     this.gameRepository = gameRepository;
   }
 
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     return this.gameRepository.findAll().stream().map(GameMinDTO::new).toList();
+  }
+
+  @Transactional(readOnly = true)
+  public GameDTO findById(Long id) {
+    var game = this.gameRepository.findById(id).orElseThrow(RuntimeException::new);
+    return new GameDTO(game);
   }
 }
