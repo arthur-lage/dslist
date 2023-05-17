b4b75d24bba82fdd57f70b514f653ff3e1aafb51
Arthur Lage <arthurlage2006@gmail.com>
2023-05-17 14:07:39 -0300

    remove unecessary annotation

--- src/main/java/com/arthur/dslist/controllers/GameController.java
+++ src/main/java/com/arthur/dslist/controllers/GameController.java
@@ -2,7 +2,6 @@ package com.arthur.dslist.controllers;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,6 @@ import com.arthur.dslist.services.GameService;
 
 @RestController
 @RequestMapping("/api/games")
-@CrossOrigin(origins = "*")
 public class GameController {
   private final GameService gameService;
 
